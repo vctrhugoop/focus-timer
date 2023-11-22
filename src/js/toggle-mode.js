96ea097a44c36019dd6fbbff11705d0d196b420a
vctrhugoop <vhdestroy@hotmail.com>
2023-11-22 19:43:19 -0300

    ✨ feat:
    add toggle mode script

---
+++ src/js/toggle-mode.js
@@ -0,0 +1,12 @@
+let darkMode = true;
+const buttonToggle = document.getElementById('toggle-mode');
+
+buttonToggle.addEventListener('click', e => {
+  document.documentElement.classList.toggle('light');
+
+  const mode = darkMode ? 'Light' : 'Dark';
+
+  e.currentTarget.querySelector('span').textContent = `${mode} mode activated`;
+
+  darkMode = !darkMode;
+});
