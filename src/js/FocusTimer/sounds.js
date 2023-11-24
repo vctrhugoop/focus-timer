5e80365ebab8698eecaabda0be08064739ad1c04
vctrhugoop <vhdestroy@hotmail.com>
2023-11-24 20:48:02 -0300

    🐳 chore: move folder

--- src/js/FocusTimer/sounds.js
+++ src/js/FocusTimer/sounds.js
@@ -1,9 +1,9 @@
-export const buttonPress = new Audio('../../assets/button-press.wav');
-export const kitchenTimer = new Audio('../../assets/kitchen-timer.mp3');
-export const florest = new Audio('../../assets/florest.wav');
-export const rain = new Audio('../../assets/rain.wav');
-export const coffeeShop = new Audio('../../assets/coffee-shop.wav');
-export const fireplace = new Audio('../../assets/fireplace.wav');
+export const buttonPress = new Audio('./assets/button-press.wav');
+export const kitchenTimer = new Audio('./assets/kitchen-timer.mp3');
+export const florest = new Audio('./assets/florest.wav');
+export const rain = new Audio('./assets/rain.wav');
+export const coffeeShop = new Audio('./assets/coffee-shop.wav');
+export const fireplace = new Audio('./assets/fireplace.wav');
 
 florest.loop = true;
 rain.loop = true;
