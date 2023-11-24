8fdd0bc1916ad5e14057043eb545f00b53dba83c
vctrhugoop <vhdestroy@hotmail.com>
2023-11-24 20:36:45 -0300

    🐞 fix: adjusts sounds

--- src/js/FocusTimer/sounds.js
+++ src/js/FocusTimer/sounds.js
@@ -1,9 +1,9 @@
-export const buttonPress = new Audio('./src/assets/button-press.wav');
-export const kitchenTimer = new Audio('./src/assets/kitchen-timer.mp3');
-export const florest = new Audio('./src/assets/florest.wav');
-export const rain = new Audio('./src/assets/rain.wav');
-export const coffeeShop = new Audio('./src/assets/coffee-shop.wav');
-export const fireplace = new Audio('./src/assets/fireplace.wav');
+export const buttonPress = new Audio('../../assets/button-press.wav');
+export const kitchenTimer = new Audio('../../assets/kitchen-timer.mp3');
+export const florest = new Audio('../../assets/florest.wav');
+export const rain = new Audio('../../assets/rain.wav');
+export const coffeeShop = new Audio('../../assets/coffee-shop.wav');
+export const fireplace = new Audio('../../assets/fireplace.wav');
 
 florest.loop = true;
 rain.loop = true;
